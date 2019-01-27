a2129f0f5d50fdbccbb1d0b862df2168e7e33ad9
Maxim Kochukov <mako0216@netcracker.com>
2019-01-28 02:15:57 +0300

    Added some folders to gitignore. Added file with solution to remove sections.

---
+++ dbRemoveTermsAndPolicy.php
@@ -0,0 +1,5 @@
+// Update language_id for not needed sections
+
+update oc_information_description set language_id = 10 where information_id = 5;
+
+update oc_information_description set language_id = 10 where information_id = 3;
