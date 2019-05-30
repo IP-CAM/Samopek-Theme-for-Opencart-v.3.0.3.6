86060ee7b48aec433b1ac2320acbb59e911552ae
Izomorf <Izomorf@gmail.com>
2019-05-31 00:29:35 +0300

    Added instruction to remove slideshow from top of the Home layout

--- changeLayout.sql
+++ changeLayout.sql
@@ -1,2 +1,4 @@
 --Remove carousel from the bottom
 delete from oc_layout_module where layout_id = 1 and code like '%carousel%';
+--Remove slideshow from the top
+delete from oc_layout_module where layout_id = 1 and code like '%slideshow%';
