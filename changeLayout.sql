1f011606c3388deaeb99a2cea7f53601118a34e0
Izomorf <Izomorf@gmail.com>
2019-05-31 00:26:57 +0300

    Revert "Added instruction to remove carousel from bottom of the Home layout"
    This reverts commit 81e98f27d718a44be560785be349f9de153bd968.

--- changeLayout.sql
+++ changeLayout.sql
@@ -1,4 +1,2 @@
 --Remove carousel from the bottom
 delete from oc_layout_module where layout_id = 1 and code like '%carousel%';
---Remove slideshow from the top
-delete from oc_layout_module where layout_id = 1 and code like '%slideshow%';
