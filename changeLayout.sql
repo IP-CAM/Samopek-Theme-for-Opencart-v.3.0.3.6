573fd4c1cc72bb6f69e53b3eb56a1df9d0a594b9
Izomorf <Izomorf@gmail.com>
2019-06-04 01:01:39 +0300

    A lot of changes. Almost finished first screen

--- changeLayout.sql
+++ changeLayout.sql
@@ -2,3 +2,12 @@
 delete from oc_layout_module where layout_id = 1 and code like '%carousel%';
 --Remove slideshow from the top
 delete from oc_layout_module where layout_id = 1 and code like '%slideshow%';
+
+--Disable english language. Enable Russian. Make sure l18n patch was copied already.
+update oc_language set sort_order = 2 where code = "en-gb";
+update oc_language set status = 0 where code = "en-gb";
+insert into oc_language set name = "Russian", code = "ru-ru", locale = "ru_RU.UTF-8,ru_RU,russian", image = "ru.png", directory = "russian", sort_order = 1, status = 1;
+
+--Update language settings for store 0
+update oc_setting set value = 'ru-ru' where `key`='config_language' and store_id = 0;
+update oc_setting set value = 'ru-ru' where `key`='config_admin_language' and store_id = 0;
