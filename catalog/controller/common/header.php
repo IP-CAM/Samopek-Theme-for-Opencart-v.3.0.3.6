e8857e9219cdb4d83013e834bf3690e8fc8b5431
Izomorf <Izomorf@gmail.com>
2019-07-17 01:43:30 +0300

    Added customer greeting + styles in header

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -77,6 +77,8 @@ class ControllerCommonHeader extends Controller {
 		$data['cart'] = $this->load->controller('common/cart');
 		$data['menu'] = $this->load->controller('common/menu');
 
+		$data['customer_name'] = $this->customer->getFirstName();
+
 		return $this->load->view('common/header', $data);
 	}
 }
