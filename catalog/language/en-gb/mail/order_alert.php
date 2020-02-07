e1bd51ca2bb0fde60faaab9b77f976fd000e0ead
Izomorf <maxim.p.kochukov@gmail.com>
2020-02-07 13:34:41 +0300

    SAM-80 - [SamopekTheme] Отображать информацию о клиенте в письме про заказ Displayed Name,surname,phone,email.

---
+++ catalog/language/en-gb/mail/order_alert.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_subject']      = '%s - Order %s';
+$_['text_received']     = 'You have received an order.';
+$_['text_order_id']     = 'Order ID:';
+$_['text_date_added']   = 'Date Added:';
+$_['text_order_status'] = 'Order Status:';
+$_['text_product']      = 'Products';
+$_['text_total']        = 'Totals';
+$_['text_comment']      = 'The comments for your order are:';
+
+// mako
+$_['text_customer']      = 'Customer:';
+$_['text_customer_name'] = 'Name: ';
+$_['text_customer_phone'] = 'Phone number: ';
+$_['text_customer_email'] = 'Email: ';
---
+++ catalog/language/ru-ru/mail/order_alert.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_subject']      = '%s - Заказ %s';
+$_['text_received']     = 'Вы получили заказ.';
+$_['text_order_id']     = '№ заказа:';
+$_['text_date_added']   = 'Дата заказа:';
+$_['text_order_status'] = 'Состояние заказа:';
+$_['text_product']      = 'Товары';
+$_['text_total']        = 'Итого';
+$_['text_comment']      = 'Комментарий к Вашему заказу:';
+
+// mako
+$_['text_customer']      = 'Клиент:';
+$_['text_customer_name'] = 'Имя: ';
+$_['text_customer_phone'] = 'Номер телефона: ';
+$_['text_customer_email'] = 'Email: ';
+?>
