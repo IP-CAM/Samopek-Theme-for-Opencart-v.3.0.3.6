7bb3f21fd9885c11fd4df1432ab245577a63cb93
Izomorf <maxim.p.kochukov@gmail.com>
2020-04-30 00:40:06 +0300

    SAM-91 - Убрать .00 в ценах на карточке товара добавлен правильный namespace + фикс для приватного массива валют

--- system/library/override/samopek/system/library/cart/currency.php
+++ system/library/override/samopek/system/library/cart/currency.php
@@ -1,8 +1,14 @@
 <?php
+namespace Cart;
 class samopek_Currency extends Currency {
+
+    private $currencies = array();
 
     /* override method */
     public function format($number, $currency, $value = '', $format = true) {
+        /* MAKO change start */
+        $this->getCurrencies();
+        /* MAKO change end */
         $symbol_left = $this->currencies[$currency]['symbol_left'];
         $symbol_right = $this->currencies[$currency]['symbol_right'];
         $decimal_place = $this->currencies[$currency]['decimal_place'];
@@ -39,5 +45,23 @@ class samopek_Currency extends Currency {
 
         return $string;
     }
+
+    // ToDo: raise a ticket as $currencies is private!!!
+    /* MAKO change start */
+    private function getCurrencies() {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency");
+
+        foreach ($query->rows as $result) {
+            $this->currencies[$result['code']] = array(
+                'currency_id'   => $result['currency_id'],
+                'title'         => $result['title'],
+                'symbol_left'   => $result['symbol_left'],
+                'symbol_right'  => $result['symbol_right'],
+                'decimal_place' => $result['decimal_place'],
+                'value'         => $result['value']
+            );
+        }
+    }
+    /* MAKO change end */
 }
 ?>
