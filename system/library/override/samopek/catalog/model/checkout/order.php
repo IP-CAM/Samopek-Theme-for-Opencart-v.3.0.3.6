ce5ec54de136aeb432c277ea886fa2ceccfe9e2c
Maxim Kochukov <maxim.kochukov@netcracker.com>
2020-08-19 22:47:06 +0300

    Removed not needed order.php. Removed logs from product.php. Added missed config files with ToDo.

--- system/library/override/samopek/catalog/model/checkout/order.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Samopek_ModelCheckoutOrder extends ModelCheckoutOrder {
-    public function addOrder($data) {
-        if (!isset($data['comment'])) {
-            $data['comment'] = "";
-        }
-        parent::addOrder($data);
-    }
-}
